9dd360c8af425276a6ed4f9ebbbebb7fa540131a
dilyarakhabibullina <khdilyara@rambler.ru>
2019-03-11 23:58:32 +0300

    all tests passed! and everything works

--- src/test/java/ru/itpark/InitialServiceTest.java
+++ src/test/java/ru/itpark/InitialServiceTest.java
@@ -1,12 +1,42 @@
 package ru.itpark;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InitialServiceTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvFileSource(resources = "/initials.csv", numLinesToSkip = 1)
+    void getInitials (String input, String expected) {
+        InitialService service = new InitialService();
+        String actual = service.getInitials(input);
+        assertEquals(expected, actual);
+}
+
+  /* @ParameterizedTest
+    @CsvFileSource(resources = "/initials.csv", numLinesToSkip = 1)
+    void getInitials(String input, String expected) {
+        InitialsService service = new InitialsService();
+
+        String actual = service.getInitials(input);
+
+        assertEquals(expected, actual);
+    }*/
+
+
+    //  @Test
+
+       //void initials1() {
+        //InitialService service = new InitialService();
+        //String actual =;
+        //String expected =;
+        //assertEquals(expected, actual);
+
+
+  /*  @Test
     void initials1() {
         InitialService service = new InitialService();
         String actual = service.Initials1("Dilyara Khabibullina");
