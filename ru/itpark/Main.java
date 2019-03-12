c9239c3a04c6b217fdd6da42359e5aae09cc144b
dilyarakhabibullina <khdilyara@rambler.ru>
2019-03-12 13:46:24 +0300

    Main added

---
+++ ru/itpark/Main.java
@@ -0,0 +1,9 @@
+package ru.itpark;
+
+public class Main {
+    public static void main(String[] args) {
+        InitialService service = new InitialService();
+        String resultFinal = service.getInitials ("Dilyara Khabibullina");//а как мне сделать, чтобы 
+        System.out.println(resultFinal);}
+            //return resultFinal;}
+}
