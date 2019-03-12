2a7e96bdfa9b9ceab24fdc6f6020ea2cc8ed8aac
dilyarakhabibullina <khdilyara@rambler.ru>
2019-03-12 08:30:37 +0300

    all tests passed! and everything works. Code and test are ready.

--- src/test/java/ru/itpark/InitialServiceTest.java
+++ src/test/java/ru/itpark/InitialServiceTest.java
@@ -10,39 +10,12 @@ class InitialServiceTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/initials.csv", numLinesToSkip = 1)
-    void getInitials (String input, String expected) {
-        InitialService service = new InitialService();
-        String actual = service.getInitials(input);
-        assertEquals(expected, actual);
-}
-
-  /* @ParameterizedTest
-    @CsvFileSource(resources = "/initials.csv", numLinesToSkip = 1)
     void getInitials(String input, String expected) {
-        InitialsService service = new InitialsService();
-
-        String actual = service.getInitials(input);
-
-        assertEquals(expected, actual);
-    }*/
-
-
-    //  @Test
-
-       //void initials1() {
-        //InitialService service = new InitialService();
-        //String actual =;
-        //String expected =;
-        //assertEquals(expected, actual);
-
-
-  /*  @Test
-    void initials1() {
         InitialService service = new InitialService();
-        String actual = service.Initials1("Dilyara Khabibullina");
-        String expected = "DK";
+        String actual = service.getInitials(input);
         assertEquals(expected, actual);
     }
+}
 
 
     {
